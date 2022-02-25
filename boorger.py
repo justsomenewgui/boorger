9037b8c5901f421bd76409f4e0e7fb4ba1657314
justsomenewgui <98216842+justsomenewgui@users.noreply.github.com>
2022-02-25 20:45:02 +0700

    Add files via upload

--- boorger.py
+++ boorger.py
@@ -1,11 +1,30 @@
 #!/usr/bin/env python3
+"""
+    Boorger. Downloads images and videos from Gelbooru, Danbooru, Konachan
+    and iibooru.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>
+"""
 
 import argparse
 from tools.startburger import Boorger
 
-parser = argparse.ArgumentParser(description="""Downloads images and
-videos from Gelbooru, Danbooru, Konachan and iibooru.""")
-parser.add_argument("url", help="""Valid url. For example:
+parser = argparse.ArgumentParser(
+    usage="""./boorger.py url [optional arguments]""",
+    description="""Downloads images and videos from Gelbooru, Danbooru,
+    Konachan and iibooru.""")
+parser.add_argument("url", nargs='?', help="""Valid url. For example:
 'https://gelbooru.com/index.php?page=post&s=list&tags=suigintou'""")
 parser.add_argument("-d", "--directory", type=str,
                     default=None, help="""Directory to store
@@ -16,6 +35,8 @@ parser.add_argument("-p", "--pages", type=int, default=1, help="""
 Number of pages you want to download.""")
 parser.add_argument("-r", "--reverse", action="store_true", help="""
 If selected, pages will be loaded in descending order.""")
+parser.add_argument("-l", "--license", action="store_true", help="""
+Print the license of boorger.""")
 args = parser.parse_args()
 url = args.url
 directory = args.directory
@@ -24,7 +45,11 @@ if args.reverse:
     reverse = True
 else:
     reverse = False
-
-#Start "startburger.py" from command line.
-download = Boorger(url, directory, reverse)
-download.loader(pages)
+if args.license:
+    with open('LICENSE') as f:
+        license = f.read()
+        print(license)
+else:
+    #Start "startburger.py" from command line.
+    download = Boorger(url, directory, reverse)
+    download.loader(pages)
